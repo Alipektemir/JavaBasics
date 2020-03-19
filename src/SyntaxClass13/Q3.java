3e940d2e88d2d77a2562b1c293e80aaa30ec01fc
Alipektemir <Alipektemir>
2020-03-18 21:36:22 -0400

    Q3

---
+++ src/SyntaxClass13/Q3.java
@@ -0,0 +1,23 @@
+package SyntaxClass13;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//write jp print 10 numbers of Fibonacci series
+		//0 1 1 2 3 5 8 13 21
+		int a,b,c;
+		a=0;
+		b=1;
+		for(int i=1; i<=10; i++) {
+			System.out.print(a+" ");
+			c=a+b;
+			a=b;
+			b=c;
+			
+		}
+		
+		
+	}
+
+}
