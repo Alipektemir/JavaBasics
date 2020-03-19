b28cd0a33210c204f43bd3de4d03b19ec0c47d29
Alipektemir <Alipektemir>
2020-03-18 20:32:02 -0400

    IQ1

--- src/SyntaxClass13/class13.java
+++ src/SyntaxClass13/class13.java
@@ -1,5 +1,23 @@
 package SyntaxClass13;
 
 public class class13 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("The value of a ="+a);
+		System.out.println("The value of b ="+b);
+		
+		
+		String str1="Day";
+		String str2="Night";
+		
+		str1=str1+str2;
+		str2=str1.substring(0,3);
+		str1=str1.substring(3);
+		System.out.println("The value of str1="+str1);
+		System.out.println("The value of str2="+str2);	}
 
 }
